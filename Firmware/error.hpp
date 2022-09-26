dc290afde4799ed857f2d13490aa26213085d8bd
Jefferson <44975876+ZX-80@users.noreply.github.com>
2022-09-26 12:15:00 -0400

    Fixed visual bug
    File was renamed to *hpp to conform with other header files. A user reported seeing an error code 3-4-4, when it should have been 4-4-4. A 1 second delay has been added to resolve this.

--- Firmware/error.hpp
+++ Firmware/error.hpp
@@ -1,12 +1,13 @@
-/** \file error.h
- *  \brief Debug/error related code
+/** \file error.hpp
+ *
+ * \brief Blink code functionality (same concept as beep codes)
  */
 
 #pragma once
 
 namespace BLINK {
-    constexpr uint8_t OVERCLOCK_FAILED = 3;
-    constexpr uint8_t NO_VALID_FILES = 4;
+    inline constexpr uint8_t OVERCLOCK_FAILED = 3;
+    inline constexpr uint8_t NO_VALID_FILES = 4;
 }
 
 /*! \brief Blink an error code to the LED. Useful for simple debugging
@@ -16,9 +17,10 @@ namespace BLINK {
  */
 void blink_code(uint8_t code, uint8_t repeat=3) {
     gpio_put(LED_BUILTIN, false);
+    sleep_ms(1000);
     for (uint8_t j = 0; j < repeat; j++) {
         for (uint8_t i = 0; i < 2 * code; i++) {
-            gpio_xor_mask(1ul << LED_BUILTIN);
+            gpio_xor_mask(1ul << LED_BUILTIN);   // Toggle the LED
             sleep_ms(250);
         }
         sleep_ms(1000);
